ab7bd3c8f9ac3feaa16e60f76ac63dfc1c78973b
DaniaLD <danial.6274@gmail.com>
2018-05-02 12:17:27 +0430

    added basic files and programmed basic socket.io

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const socket = require('socket.io');
+const app = express();
+const port = process.env.PORT || 3000;
+const http = require('http').Server(app);
+
+
+app.set('view engine', 'ejs'); // Set View Engine
+app.use(express.static(__dirname + '/public')); // Set Public Folder
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Socket Setup
+const io = socket(http);
+
+io.on('connection', socket => {
+    console.log(`A new user connected, Online users: ${socket.conn.server.clientsCount}`);
+
+    socket.on('disconnect', () => {
+        console.log(`A user disconnected, Online users: ${socket.conn.server.clientsCount}`);
+    });
+});
+
+http.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});
